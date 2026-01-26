760a4e9ddd0b28e2abba81f2fed3f90ef7251f88
abi <a.thamma@uva.nl>
2026-01-26 23:50:03 +0100

    Add SLURM script for parallel VGG16 training experiments

---
+++ scripts/slurm/run_experiment_parallel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=4
+#SBATCH --cpus-per-task=72
+#SBATCH --partition=gpu_a100
+#SBATCH --time=02:00:00
+#SBATCH --output=/home/%u/logs/%j.log
+#SBATCH --error=/home/%u/logs/%j.log
+
+VENV_PATH="/home/athamma1/Projects/viewpoint-diverse-training-diet/.venv"
+SCRIPT_PATH="/home/athamma1/Projects/viewpoint-diverse-training-diet/scripts/experiments/vgg_v1.py"
+
+source "${VENV_PATH}/bin/activate"
+
+torchrun --nnodes=1 --nproc_per_node=4  "${SCRIPT_PATH}"
+
